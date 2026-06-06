978d4c55f45165d0a84d5853f2fae369be4eca9c
julijadigital <julijadim76@gmail.com>
2026-06-06 23:39:41 +0200

    Update and rename src/vite.config.ts to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
-
-~/project
-❯ cat vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +7,3 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
 });
-
-~/project
-❯ 
