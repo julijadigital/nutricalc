b43595d98c3e5ba64a87beafe9126920f33b1619
julijadigital <julijadim76@gmail.com>
2026-06-06 23:27:19 +0200

    Create vite.config.ts

---
+++ src/vite.config.ts
@@ -0,0 +1,16 @@
+
+~/project
+❯ cat vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
+
+~/project
+❯ 
